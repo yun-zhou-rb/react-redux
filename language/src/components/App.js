368796091a5ed54dbfd62759dab5118ebe1932e6
yun zhou <yun.zhou@redbubble.com>
2019-01-25 17:57:38 +1100

    react context

---
+++ language/src/components/App.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+
+import UserCreate from "./UserCreate";
+import LanguageContext from '../contexts/LanguageContext';
+import ColorContext from '../contexts/ColorContext';
+
+class App extends React.Component {
+  state = {language: 'english'};
+  onLanguageChange = (lang) => {
+    this.setState({language: lang});
+  }
+
+  render() {
+    return (
+      <div className="ui container">
+        <div>
+          Select a language:
+          <i className="flag us" onClick={() => this.onLanguageChange('english')}/>
+          <i className="flag cn" onClick={() => this.onLanguageChange('chinese')}/>
+        </div>
+        <ColorContext.Provider value="primary">
+          <LanguageContext.Provider value={this.state.language}>
+            <UserCreate/>
+          </LanguageContext.Provider>
+        </ColorContext.Provider>
+      </div>
+    );
+
+  }
+}
+
+export default App;
---
+++ language/src/components/Button.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import LanguageContext from '../contexts/LanguageContext';
+import ColorContext from '../contexts/ColorContext';
+
+class Button extends React.Component {
+  renderButton(color) {
+    return (<button className={`ui button ${color}`}>
+        <LanguageContext.Consumer>
+          {(value) => value === "english" ? "Submit" : "提交"}
+        </LanguageContext.Consumer>
+      </button>
+    );
+  }
+
+  render() {
+
+    return (
+      <ColorContext.Consumer>
+        {(color) => this.renderButton(color)}
+
+      </ColorContext.Consumer>
+    );
+  }
+}
+
+export default Button;
